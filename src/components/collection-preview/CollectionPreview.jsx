f0f20944dd4627ebb13ee2f2082252953a4607fa
Nicholas de Waal <nick.a.dewaal@gmail.com>
2021-03-10 21:02:40 +0900

    updating our app with a shop page, collection preview component nad collection item component

--- src/components/collection-preview/CollectionPreview.jsx
+++ src/components/collection-preview/CollectionPreview.jsx
@@ -1,22 +1,20 @@
 import React from 'react'
+
 import CollectionItem from '../collection-item/CollectionItem'
+
 import './collectionPreview.scss'
 
-// style
-import './collectionPreview.scss'
-
-const CollectionPreview = ({title, items}) => {
-  return (
-    <div className="collection-preview">
-      <h1 className="title">{title.toUpperCase()}</h1>
-      <div className="preview"></div>
-      {
-        items.filter((item, i) => i < 4).map(({id, ...itemOtherProps}) => (
-          <CollectionItem key={id} {...itemOtherProps}/>  
-        ))
-      }
+const CollectionPreview = ({ title, items }) => (
+  <div className='collection-preview'>
+    <h1 className='title'>{title.toUpperCase()}</h1>
+    <div className='preview'>
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
+        ))}
     </div>
-  )
-}
+  </div>
+)
 
 export default CollectionPreview
--- src/pages/shop/ShopPage.jsx
+++ src/pages/shop/ShopPage.jsx
@@ -1,16 +1,17 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import CollectionPreview from '../../components/collection-preview/CollectionPreview'
 import SHOP_DATA from './shop-data'
 
 const ShopPage = () => {
   const [collections] = useState(SHOP_DATA)
   return (
-    <div>
-      {
-        collections.map(({id, ...otherCollectionProps}) => (
-          <CollectionPreview key={id} {...otherCollectionProps}></CollectionPreview>
-        ))
-      }
+    <div className='shop-page'>
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview
+          key={id}
+          {...otherCollectionProps}
+        ></CollectionPreview>
+      ))}
     </div>
   )
 }
