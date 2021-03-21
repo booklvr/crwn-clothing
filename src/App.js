b209831d481847c35f282082b2ea4cd790350cfd
Nicholas de Waal <nick.a.dewaal@gmail.com>
2021-03-21 22:31:14 +0900

    working on firebase

--- src/App.js
+++ src/App.js
@@ -1,74 +1,167 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
-// import { connect } from 'react-redux'
-import { useDispatch, useSelector } from 'react-redux'
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
 
-// PAGES
+import './App.css'
+
 import HomePage from './pages/homepage/HomePage'
 import ShopPage from './pages/shopPage/ShopPage'
 import SignInAndSignUpPage from './pages/sign-in-sign-up/SignInSignUp'
 import CheckoutPage from './pages/checkoutPage/CheckoutPage'
 
-// COMPONENTS
 import Header from './components/header/Header'
 
-// UTILS
-import { auth, createUserProfileDocument } from './firebase/firebase.utils'
+import {
+  auth,
+  createUserProfileDocument,
+} from './firebase/firebase.utils'
 
-// ACTIONS
+// selectors
 import { setCurrentUser } from './redux/user/user.actions'
-
-// SELECTORS
 import { selectCurrentUser } from './redux/user/user.selectors'
 
-// STYLE
-import './App.css'
+class App extends React.Component {
+  unsubscribeFromAuth = null
 
-// class App extends React.Component {
-const App = () => {
-  const dispatch = useDispatch()
-  const [userAuth, setUserAuth] = useState(null)
-  const currentUser = useSelector(selectCurrentUser)
+  componentDidMount() {
+    const { setCurrentUser } = this.props
 
-  useEffect(() => {
-    const unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth)
 
-        setUserAuth(userAuth)
         userRef.onSnapshot((snapShot) => {
-          dispatch(
-            setCurrentUser({
-              id: snapShot.id,
-              ...snapShot.data(),
-            })
-          )
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data(),
+          })
         })
-      } else {
-        dispatch(setCurrentUser(null))
       }
+
+      setCurrentUser(userAuth)
     })
+  }
 
-    return unsubscribeFromAuth
-  }, [dispatch])
+  componentWillUnmount() {
+    this.unsubscribeFromAuth()
+  }
 
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route
-          exact
-          path='/signin'
-          render={() =>
-            currentUser ? <Redirect to='/' /> : <SignInAndSignUpPage />
-          }
-        />
-        <Route exact path='/checkout' component={CheckoutPage} />
-      </Switch>
-    </div>
-  )
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
+          <Route
+            exact
+            path='/signin'
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to='/' />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+          />
+        </Switch>
+      </div>
+    )
+  }
 }
 
-export default App
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
+
+// import React, { useEffect, useState } from 'react'
+// import { Switch, Route, Redirect } from 'react-router-dom'
+// // import { connect } from 'react-redux'
+// import { useDispatch, useSelector } from 'react-redux'
+
+// // PAGES
+// import HomePage from './pages/homepage/HomePage'
+// import ShopPage from './pages/shopPage/ShopPage'
+// import SignInAndSignUpPage from './pages/sign-in-sign-up/SignInSignUp'
+// import CheckoutPage from './pages/checkoutPage/CheckoutPage'
+
+// // COMPONENTS
+// import Header from './components/header/Header'
+
+// // UTILS
+// import {
+//   auth,
+//   createUserProfileDocument,
+//   addCollectionAndDocuments,
+// } from './firebase/firebase.utils'
+
+// // ACTIONS
+// import { setCurrentUser } from './redux/user/user.actions'
+
+// // SELECTORS
+// import { selectCurrentUser } from './redux/user/user.selectors'
+// import { selectCollectionsForPreview } from './redux/shop/shop.selectors'
+
+// // STYLE
+// import './App.css'
+
+// // class App extends React.Component {
+// const App = () => {
+//   const dispatch = useDispatch()
+//   const [userAuth, setUserAuth] = useState(null)
+//   const currentUser = useSelector(selectCurrentUser)
+//   const collectionsArray = useSelector(selectCollectionsForPreview)
+
+//   console.log('collectionsArray', collectionsArray)
+
+//   useEffect(() => {
+//     const unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+//       if (userAuth) {
+//         const userRef = await createUserProfileDocument(userAuth)
+
+//         setUserAuth(userAuth)
+//         userRef.onSnapshot((snapShot) => {
+//           dispatch(
+//             setCurrentUser({
+//               id: snapShot.id,
+//               ...snapShot.data(),
+//             })
+//           )
+//         })
+//       } else {
+//         dispatch(setCurrentUser(null))
+//       }
+//     })
+//     dispatch(addCollectionAndDocuments('collections', collectionsArray))
+
+//     return unsubscribeFromAuth
+//   }, [dispatch, collectionsArray])
+
+//   return (
+//     <div>
+//       <Header />
+//       <Switch>
+//         <Route exact path='/' component={HomePage} />
+//         <Route path='/shop' component={ShopPage} />
+//         <Route
+//           exact
+//           path='/signin'
+//           render={() =>
+//             currentUser ? <Redirect to='/' /> : <SignInAndSignUpPage />
+//           }
+//         />
+//         <Route exact path='/checkout' component={CheckoutPage} />
+//       </Switch>
+//     </div>
+//   )
+// }
+
+// export default App
--- src/pages/shopPage/ShopPage.jsx
+++ src/pages/shopPage/ShopPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Route } from 'react-router-dom'
 
 // PAGES
@@ -7,7 +7,21 @@ import CollectionPage from '../collectionPage/CollectionPage'
 // COMPONENTS
 import CollectionsOverview from '../../components/collections-overview/CollectionsOverview'
 
+// UTILS
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from '../../firebase/firebase.utils'
+
 const ShopPage = ({ match }) => {
+  useEffect(() => {
+    const collectionRef = firestore.collection('collections')
+    collectionRef.onSnapshot(async (snapshot) => {
+      convertCollectionsSnapshotToMap(snapshot)
+    })
+    // return unsubscribeFromSnapshot()
+  }, [])
+
   return (
     <div className='shop-page'>
       <Route exact path={`${match.path}`} component={CollectionsOverview} />
